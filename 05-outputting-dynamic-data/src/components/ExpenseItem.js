9d091fd8c218ff42c163b39d83bad0e64bc2d3d2
Jeremy Krovitz CW <jkrovitz.cw@mmm.com>
2022-05-09 14:27:04 -0500

    Add code through video 36
    Added code through video 36. Outputting Dynamic Data & Working with
    Expressions inh JSX.

---
+++ 05-outputting-dynamic-data/src/components/ExpenseItem.js
@@ -0,0 +1,19 @@
+import './ExpenseItem.css';
+
+const expenseDate = new Date(2021, 2, 28);
+const expenseTitle = 'Car Insurance';
+const expenseAmount = 294.67;
+
+function ExpenseItem() {
+  return (
+    <div className="expense-item">
+      <div>{expenseDate.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
