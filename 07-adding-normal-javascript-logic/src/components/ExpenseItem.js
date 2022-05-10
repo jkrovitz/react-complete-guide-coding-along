469add45137e1ad5a6eae0003ec8ce09f378e776
jkrovitz <jeremy@jeremykrovitz.com>
2022-05-10 14:16:52 -0500

    Add code through video 38
    Added code through video 38. Adding "normal" JavaScript Logic to
    Components

---
+++ 07-adding-normal-javascript-logic/src/components/ExpenseItem.js
@@ -0,0 +1,28 @@
+import './ExpenseItem.css';
+
+const expenseDate = new Date(2021, 2, 28);
+const expenseTitle = 'Car Insurance';
+const expenseAmount = 294.67;
+
+function ExpenseItem(props) {
+
+  const month = props.date.toLocaleString('en-US', {month: 'long'});
+  const day = props.date.toLocaleString('en-US', {day: '2-digit'});
+  const year = props.date.getFullYear();
+
+  return (
+    <div className="expense-item">
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
