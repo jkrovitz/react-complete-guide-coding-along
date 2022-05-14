1650caf4d96705071ad3fd139686ae429404b90f
jkrovitz <jeremy@jeremykrovitz.com>
2022-05-14 13:58:04 -0500

    Add code through video 44
    Added code through video 44. An Alternative Function Syntax.

---
+++ 11-finished/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react'; // Technically we don't need this import but this is how it was done in the past.
+import ReactDOM from 'react-dom/client';
+
+import './index.css';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
