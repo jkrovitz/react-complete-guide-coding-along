99c393dbe9565fbe0c58c375075e7a9e3ca281e1
jkrovitz <jeremy@jeremykrovitz.com>
2022-05-18 10:45:06 -0500

    Add code through video 48
    Added code through video 48. How Component Functions Are Executed.

--- 01-working-with-state/src/components/Expenses/ExpenseItem.js
+++ 01-working-with-state/src/components/Expenses/ExpenseItem.js
@@ -5,15 +5,18 @@ import './ExpenseItem.css';
 
 const ExpenseItem = props => {
 
+  let title = props.title;
+
   const clickHandler = () => {
-      console.log('Clicked!');
+      title = 'Updated!';
+      console.log(title);
   };
   
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
-        <h2>{props.title}</h2> {/* trying to make output of props.title change when button is clicked */}
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
       <button onClick={clickHandler}>Change Title</button>
