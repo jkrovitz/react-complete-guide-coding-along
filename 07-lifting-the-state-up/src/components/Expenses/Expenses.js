32c2462f7ff4f7d3d03dca40d48872e3dce6fd0b
jkrovitz <jeremy@jeremykrovitz.com>
2022-09-06 15:26:18 -0500

    Add folder 07-lifting-the-state-up

---
+++ 07-lifting-the-state-up/src/components/Expenses/Expenses.js
@@ -0,0 +1,37 @@
+import React from "react";
+import ExpenseItem from "./ExpenseItem";
+import ExpensesFilter from "./ExpensesFilter";
+import Card from "../UI/Card";
+import "./Expenses.css";
+
+const Expenses = (props) => {
+  return (
+    <div>
+      <ExpensesFilter />
+      <Card className="expenses">
+        <ExpenseItem
+          title={props.expenses[0].title}
+          amount={props.expenses[0].amount}
+          date={props.expenses[0].date}
+        />
+        <ExpenseItem
+          title={props.expenses[1].title}
+          amount={props.expenses[1].amount}
+          date={props.expenses[1].date}
+        />
+        <ExpenseItem
+          title={props.expenses[2].title}
+          amount={props.expenses[2].amount}
+          date={props.expenses[2].date}
+        />
+        <ExpenseItem
+          title={props.expenses[3].title}
+          amount={props.expenses[3].amount}
+          date={props.expenses[3].date}
+        />
+      </Card>
+    </div>
+  );
+};
+
+export default Expenses;
