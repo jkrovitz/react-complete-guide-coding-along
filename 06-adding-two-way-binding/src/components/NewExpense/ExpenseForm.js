1cfc18eafc1fe58679d5e30b6407594a509cfc68
jkrovitz <jeremy@jeremykrovitz.com>
2022-09-06 09:27:06 -0500

    Add part 1 of code for video 59
    Added part 1 of code for video 59. Child-to-Parent Component
    Communication (Bottom-up).

--- 06-adding-two-way-binding/src/components/NewExpense/ExpenseForm.js
+++ 06-adding-two-way-binding/src/components/NewExpense/ExpenseForm.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 
 import "./ExpenseForm.css";
 
-const ExpenseForm = () => {
+const ExpenseForm = (props) => {
   const [enteredTitle, setEnteredTitle] = useState("");
   const [enteredAmount, setEnteredAmount] = useState("");
   const [enteredDate, setEnteredDate] = useState("");
