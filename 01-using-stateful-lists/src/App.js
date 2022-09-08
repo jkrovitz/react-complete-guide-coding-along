2e053ff538e08e085b839d06e8ffdfd3ec3d9476
jkrovitz <jeremy@jeremykrovitz.com>
2022-09-08 13:34:53 -0500

    Update notes and add code through video 64
    Added notes and code from video 64. Rendering Lists of Data.

--- 01-using-stateful-lists/src/App.js
+++ 01-using-stateful-lists/src/App.js
@@ -34,7 +34,7 @@ const App = () => {
   return (
     <div>
       <NewExpense onAddExpense={addExpenseHandler}/>
-      <Expenses expenses={expenses} />
+      <Expenses items={expenses} />
     </div>
   );
 };
