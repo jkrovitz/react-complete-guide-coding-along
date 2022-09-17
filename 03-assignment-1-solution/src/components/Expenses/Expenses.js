1a9bd28ffd4fa65f9febfca2f94ca3aecbae64c6
jkrovitz <jeremy@jeremykrovitz.com>
2022-09-16 21:45:20 -0500

    Add 03-assignment-1-solution folder

---
+++ 03-assignment-1-solution/src/components/Expenses/Expenses.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import ExpenseItem from "./ExpenseItem";
+import ExpensesFilter from "./ExpensesFilter";
+import Card from "../UI/Card";
+import "./Expenses.css";
+
+const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
+  const filteredItems = props.items.filter(item => item.date.getFullYear() == filteredYear);
+
+  return (
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        {filteredItems.map((expense) => (
+          <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
+      </Card>
+    </div>
+  );
+};
+
+export default Expenses;
