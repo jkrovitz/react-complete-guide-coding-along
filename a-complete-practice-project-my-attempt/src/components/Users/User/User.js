828237a1a9b21fa9f3a1ddf23a8d9cc875c415f1
jkrovitz <jeremy@jeremykrovitz.com>
2022-10-10 14:14:20 -0500

    Add User and UserList styling

--- a-complete-practice-project-my-attempt/src/components/Users/User/User.js
+++ a-complete-practice-project-my-attempt/src/components/Users/User/User.js
@@ -4,8 +4,8 @@ import styles from "./User.module.css";
 const User = (props) => {
   return (
     <li>
-      <Card className={styles["a-user"]}>
-        <p>
+      <Card className={styles.user}>
+        <p className={styles["user-details"]}>
           {props.username} ({props.age} years old)
         </p>
       </Card>
--- a-complete-practice-project-my-attempt/src/components/Users/UsersList/UsersList.js
+++ a-complete-practice-project-my-attempt/src/components/Users/UsersList/UsersList.js
@@ -1,12 +1,14 @@
 import User from "../User/User";
 
+import styles from "./UsersList.module.css";
+
 const UsersList = (props) => {
   if (props.users.length === 0) {
     return <p className="users-list__fallback">Found no users.</p>;
   }
 
   return (
-    <ul>
+    <ul className={styles["users-list"]}>
       {props.users.map((user) => (
         <User key={user.id} username={user.username} age={user.age} />
       ))}
