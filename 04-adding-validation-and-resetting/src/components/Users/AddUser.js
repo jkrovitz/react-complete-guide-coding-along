fbef07716e477830b64957454f85b6be6fee2015
jkrovitz <jeremy@jeremykrovitz.com>
2023-01-27 16:29:18 -0600

    Add some code and notes for video 93
    Add some code and notes for video 93. Adding Validation & Resetting
    Logic.

--- 04-adding-validation-and-resetting/src/components/Users/AddUser.js
+++ 04-adding-validation-and-resetting/src/components/Users/AddUser.js
@@ -11,6 +11,8 @@ const AddUser = (props) => {
   const addUserHandler = (event) => {
     event.preventDefault();
     console.log(enteredUsername, enteredAge);
+    setEnteredUsername('');
+    setEnteredAge('');
   };
 
   const usernameChangeHandler = (event) => {
@@ -25,9 +27,9 @@ const AddUser = (props) => {
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="">Username</label>
-        <input id="username" type="text" onChange={usernameChangeHandler} />
+        <input id="username" type="text" value={enteredUsername} onChange={usernameChangeHandler} />
         <label htmlFor="age">Age (Years)</label>
-        <input id="age" type="number" onChange={ageChangeHandler} />
+        <input id="age" type="number" value={enteredAge} onChange={ageChangeHandler} />
         <Button type="submit">Add User</Button>
       </form>
     </Card>
